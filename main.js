f338e8fad6ebcbc877b4abba4dc7afb112f097c0
Meagan Ramey <62218765+meaganramey@users.noreply.github.com>
2020-08-13 16:16:38 -0400

    Initialize gh-pages

---
+++ main.js
@@ -0,0 +1,10 @@
+// output the header row
+document.write('<span class="cell header">&times;</span>');
+let i = 0;
+while (i <= 10) {
+  document.write('<span class="cell header">' + i + "</span>");
+  i++;
+}
+document.write("<br>");
+
+// TODO: Write two nested while loops to output the rest of the multiplication table
