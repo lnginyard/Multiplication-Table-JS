b77ccd499ec66bccd02b588b87d2a235d2bb9ee6
kenzieadmin <kenzieadmin@users.noreply.github.com>
2020-08-28 17:39:58 +0000

    deploy: f053393028a24d53c7c045eae2c159d95cf1dc93

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // output the header row
 document.write('<span class="cell header">&times;</span>');
 let i = 0;
-while (i <= 10) {(number =
+while (i <= 10) {
   document.write('<span class="cell header">' + i + "</span>");
   i++;
 }
